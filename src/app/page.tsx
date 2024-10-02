b1d27c09f14220251acb9844375548a218f5319f
21Cash <21ThousandProof@gmail.com>
2024-10-02 17:23:30 +0530

    Initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,98 @@
-'use client';
+import {
+  getLowestRatedGuardianUserInfo,
+  getLowestRatedKnightUserInfo,
+} from '@/app/api/Utils';
+import clsx from 'clsx';
 
-import Head from 'next/head';
-import * as React from 'react';
-import '@/lib/env';
+export const revalidate = 1800;
 
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
+let lastFetched: string | undefined;
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
+async function fetchData() {
+  const guardianInfo = await getLowestRatedGuardianUserInfo();
+  const knightInfo = await getLowestRatedKnightUserInfo();
 
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
+  if (!lastFetched) {
+    lastFetched = new Date().toUTCString();
+  }
 
-export default function HomePage() {
+  return { guardianInfo, knightInfo, lastFetched };
+}
+
+export default async function MainPage() {
+  const { guardianInfo, knightInfo, lastFetched } = await fetchData();
+
   return (
-    <main>
-      <Head>
-        <title>Hi</title>
-      </Head>
-      <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
+    <main className='relative min-h-screen bg-dark text-white'>
+      <header className='absolute top-0 left-0 w-full flex justify-center mt-20'>
+        <h1 className='text-6xl font-extrabold bg-gradient-to-r from-blue-400 via-green-400 to-sky-400 text-transparent bg-clip-text animate-pulse drop-shadow-lg'>
+          21LeetcodeStats
+        </h1>
+      </header>
 
-          <ButtonLink className='mt-6' href='/components' variant='light'>
-            See all components
-          </ButtonLink>
-
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
+      <section className='flex items-center justify-center min-h-screen'>
+        <div className='flex justify-center gap-36 w-full max-w-6xl'>
+          <div className='flex flex-col items-center bg-gray-800 p-10 rounded-lg shadow-lg w-6/12'>
             <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
+              src='/Guardian.gif'
+              alt='Guardian'
+              className='w-40 h-40 mb-4'
             />
-          </UnstyledLink>
+            <h3 className='text-lg font-semibold'>Lowest Rated Guardian:</h3>
+            <a
+              href={`https://leetcode.com/${guardianInfo.username}`}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-base text-blue-400 hover:underline mt-2'
+            >
+              {guardianInfo.username}
+            </a>
+            <p className='text-base mt-2'>
+              Rating: {parseFloat(guardianInfo.userRating).toFixed(2)}
+            </p>
+            <p className='text-sm mt-2'>Last Updated: {lastFetched}</p>
+          </div>
 
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
-          </footer>
+          <div className='flex flex-col items-center bg-gray-800 p-10 rounded-lg shadow-lg w-6/12'>
+            <img src='/Knight.gif' alt='Knight' className='w-40 h-40 mb-4' />
+            <h3 className='text-lg font-semibold'>Lowest Rated Knight:</h3>
+            <a
+              href={`https://leetcode.com/${knightInfo.username}`}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-base text-blue-400 hover:underline mt-2'
+            >
+              {knightInfo.username}
+            </a>
+            <p className='text-base mt-2'>
+              Rating: {parseFloat(knightInfo.userRating).toFixed(2)}
+            </p>
+            <p className='text-sm mt-2'>Last Updated: {lastFetched}</p>
+          </div>
         </div>
       </section>
+
+      <footer className='absolute bottom-0 left-0 w-full py-6 text-center'>
+        <p className='text-sm'>
+          Made by{' '}
+          <a
+            href='https://github.com/21Cash'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-400 hover:underline'
+          >
+            21Cash
+          </a>
+        </p>
+        <a
+          href='https://github.com/21Cash/21LeetcodeStats'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-sm text-blue-400 hover:underline'
+        >
+          Link to GitHub Source Code
+        </a>
+      </footer>
     </main>
   );
 }
